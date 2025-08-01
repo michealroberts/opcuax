e568889f68844e302974906a0285e9dd9576de9a
michealroberts <michael@observerly.com>
2025-08-01 13:39:58 +0100

    feat: add ReferenceTypeId enum to opcuax module
    feat: add ReferenceTypeId enum to opcuax module

--- test/test_enum.py
+++ test/test_enum.py
@@ -7,7 +7,7 @@
 
 import unittest
 
-from opcuax.enums import NodeClass
+from opcuax.enums import NodeClass, ReferenceTypeId
 
 # **************************************************************************************
 
@@ -24,6 +24,30 @@ class TestNodeClassEnum(unittest.TestCase):
         self.assertEqual(NodeClass.ReferenceType, 32)
         self.assertEqual(NodeClass.DataType, 64)
         self.assertEqual(NodeClass.View, 128)
+
+
+# **************************************************************************************
+
+
+class TestReferenceTypeIdEnum(unittest.TestCase):
+    def test_reference_type_id_enum(self):
+        # Test that the ReferenceTypeId enum has the expected values
+        self.assertEqual(ReferenceTypeId.References, 31)
+        self.assertEqual(ReferenceTypeId.NonHierarchicalReferences, 32)
+        self.assertEqual(ReferenceTypeId.HierarchicalReferences, 33)
+        self.assertEqual(ReferenceTypeId.HasChild, 34)
+        self.assertEqual(ReferenceTypeId.Organizes, 35)
+        self.assertEqual(ReferenceTypeId.HasEventSource, 36)
+        self.assertEqual(ReferenceTypeId.HasModellingRule, 37)
+        self.assertEqual(ReferenceTypeId.HasEncoding, 38)
+        self.assertEqual(ReferenceTypeId.HasTypeDefinition, 40)
+        self.assertEqual(ReferenceTypeId.HasSubtype, 45)
+        self.assertEqual(ReferenceTypeId.HasProperty, 46)
+        self.assertEqual(ReferenceTypeId.HasComponent, 47)
+        self.assertEqual(ReferenceTypeId.HasNotifier, 48)
+        self.assertEqual(ReferenceTypeId.HasOrderedComponent, 49)
+        self.assertEqual(ReferenceTypeId.HasCause, 57)
+        self.assertEqual(ReferenceTypeId.HasEffect, 58)
 
 
 # **************************************************************************************
